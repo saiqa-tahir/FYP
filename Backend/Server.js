55f8e69fa138b80fd9223cac7cb53a2520d89bc2
iam-zohaib <f219654@cfd.nu.edu.pk>
2024-11-30 11:12:14 +0500

    new

---
+++ Backend/Server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const resumeRoutes = require('./routes/resumeRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json()); // For parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://Zohaib:Zohaib@cluster0.v7oju4g.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+// Routes
+app.use('/api/resumes', resumeRoutes);
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ FrontEnd/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import SeekerNavbar from './Components/Jobseeker/SeekerNavbar'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <SeekerNavbar/>
+   
+    </>
+  )
+}
+
+export default App
